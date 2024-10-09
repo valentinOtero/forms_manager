db0fff74a2da7a5103363be3a6c49d3905b83375
valentinOtero <166435591+valentinOtero@users.noreply.github.com>
2024-10-09 16:57:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+        dns: false,
+        child_process: false,
+        timers: false,
+        'timers/promises': false,
+      }
+    }
+    return config
+  },
+}
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
